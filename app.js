56a583b02723ddc2519144cd2f73c56e96b196df
Jang-DongHo <donghoo18@naver.com>
2022-04-21 21:10:58 +0900

    Updated getRestaurantUrl

--- app.js
+++ app.js
@@ -1,31 +1,30 @@
-// puppeteer을 가져온다.
 const puppeteer = require("puppeteer");
-// cheerio를 가져온다.
 const cheerio = require("cheerio");
 
-(async () => {
-  // 브라우저를 실행한다.
-  // 옵션으로 headless모드를 끌 수 있다.
+const getRestaurantUrl = async () => {
   const browser = await puppeteer.launch({
     headless: false,
   });
-
-  // 새로운 페이지를 연다.
   const page = await browser.newPage();
-  // 페이지의 크기를 설정한다.
   await page.setViewport({
     width: 1366,
     height: 768,
   });
 
-  const keyword = "가좌동%20돼지국밥";
-  await page.goto(`https://map.kakao.com`);
-  await page.waitForSelector('input[id="search.keyword.query"]');
+  const keyword = "가좌동 통큰돼지";
+  await page.goto(
+    `https://m.map.naver.com/search2/search.naver?query=${keyword}&sm=hty&style=v5`
+  );
   const content = await page.content();
   const $ = cheerio.load(content);
-  const input = await page.$('input[id="search"]');
-  await input.click();
-  await input.type(keyword);
-  await page.keyboard.press("Enter");
+  const restaurantCid = $(
+    "#ct > div.search_listview._content._ctList > ul > li > div.item_info > a.a_item.a_item_distance._linkSiteview"
+  ).attr("data-cid");
+  if (restaurantCid) {
+    const url = `https://m.place.naver.com/restaurant/${restaurantCid}`;
+    console.log(url);
+  }
   browser.close();
-})();
+};
+
+getRestaurantUrl();
