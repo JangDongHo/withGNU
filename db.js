fdc136c146ac74de3f69ce0a69e0cefe3be100a9
Jang-DongHo <donghoo18@naver.com>
2022-04-27 20:12:47 +0900

    csv-parser 모듈 추가

--- db.js
+++ db.js
@@ -1,22 +1,14 @@
-const dfd = require("danfojs-node");
+const fs = require("fs");
+const csv = require("csv-parser");
+let results = [];
 
-dfd
-  .readCSV("./db.csv")
-  .then((df) => {
-    let sub_df = df.loc({
-      columns: [
-        "상권업종대분류명",
-        "상호명",
-        "상권업종중분류명",
-        "상권업종소분류명",
-        "표준산업분류명",
-        "행정동명",
-        "위도",
-        "경도",
-      ],
-    });
-    sub_df.head().print();
-  })
-  .catch((err) => {
-    console.log(err);
+// DB csv -> json 변환
+fs.createReadStream("db.csv")
+  .pipe(csv())
+  .on("data", (data) => results.push(data))
+  .on("end", () => {
+    results = results.filter(
+      (value) =>
+        value.상권업종대분류명 === "음식" && value.행정동명 === "가호동"
+    );
   });
