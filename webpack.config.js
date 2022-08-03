bb86be37b7ab3ea64c7c8c810646e453186e1271
Jang-DongHo <donghoo18@naver.com>
2022-08-03 22:16:36 +0900

    webpack scss 준비

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,19 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const BASE_JS = "./src/client/js/";
 
 module.exports = {
   watch: true,
   entry: {
+    main: BASE_JS + "main.js",
     commentSection: BASE_JS + "commentSection.js",
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/styles.css",
+    }),
+  ],
   output: {
     filename: "js/[name].js",
     path: path.resolve(__dirname, "assets"),
@@ -22,6 +29,10 @@ module.exports = {
             presets: [["@babel/preset-env", { targets: "defaults" }]],
           },
         },
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
     ],
   },
