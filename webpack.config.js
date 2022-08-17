d9ebf2ddb6e42df9a8cc15faa324b4635fb2dbd6
Jang-DongHo <donghoo18@naver.com>
2022-08-17 15:48:43 +0900

    Updated home scss

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,6 @@ module.exports = {
   entry: {
     main: BASE_JS + "main.js",
     commentSection: BASE_JS + "commentSection.js",
-    slider: BASE_JS + "slider.js",
   },
   plugins: [
     new MiniCssExtractPlugin({
